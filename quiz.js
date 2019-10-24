25e526ec08484df08df9a1bc51d8f01e1c38db6a
KinshukWorkExperience <56830325+KinshukWorkExperience@users.noreply.github.com>
2019-10-24 15:27:12 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,12 @@
+/* NOTE FOR SELF 
+ * LEXICAL SCOPE IS USED BY ARROW FUNCTIONS 
+ * CONTEXTUAL SCOPE IS USED BY NORMAL FUNCTIONS 
+ * The lexical scope inherits the scope environment of the parent function that is calling the arrow function 
+ * The contextual scope inherits the scope environment of the current function that's being called 
+ * This means that the function def would not work as this does not exist in the function f(), unless you bind it, or make a variable for example self set to this which'd be from the parent scope 
+ * The arrow function would work
+*/
+
 class Apple { 
 	constructor() {
 		this.Test = 20; 
